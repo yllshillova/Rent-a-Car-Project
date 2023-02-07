28b0df8304ed951791c4d83e4c17b29227e06d69
Fazli Thaqi <fazlithaqi121@gmail.com>
2023-02-07 19:48:37 +0100

    Few buttons and forms added , ready for php...

---
+++ views/Cars.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cars</title>
+    <link rel="stylesheet" href="../css/CarsStyle.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/HomePage.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+
+    <?php
+
+    include '../components/header.php';
+
+    ?>
+
+    <section class="section2">
+        <div class="container" id="section2">
+
+            <div class="navbar2">
+                <ul class="nav-items">
+                    <li><a href="#">Available cars for rent</a></li>
+                </ul>
+            </div>
+
+
+
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto1Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto1Kategorit.jpg" />
+                        <h3 name="carName">BMW 2 Series</h3>
+                        <h1 name="price">$99,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto2Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto2Kategorit.jpg" />
+                        <h3 name="carName">Kia Rio</h3>
+                        <h1 name="price">$59,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto3Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto3Kategorit.jpg" />
+                        <h3 name="carName">Kia Forte</h3>
+                        <h1 name="price">$69,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto4Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto4Kategorit.jpg" />
+                        <h3 name="carName">Genesis G70</h3>
+                        <h1 name="price">$79,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto6Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto6Kategorit.jpg" />
+                        <h3 name="carName">Nissan Maxima</h3>
+                        <h1 name="price">$85,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto15Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto15Kategorit.jpg" />
+                        <h3 name="carName">Porsche Panamera</h3>
+                        <h1 name="price">$149,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto18Kategorite.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto18Kategorite.jpg" />
+                        <h3 name="carName">BMW M4</h3>
+                        <h1 name="price">$119,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto14Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto14Kategorit.jpg" />
+                        <h3 name="carName">Jeep Ford</h3>
+                        <h1 name="price">$69,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto13Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto13Kategorit.jpg" />
+                        <h3 name="carName">Chevrolet S240</h3>
+                        <h1 name="price">$99,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto11Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto11Kategorit.jpg" />
+                        <h3 name="carName">Lexus Jeep</h3>
+                        <h1 name="price">$99,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto10Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto10Kategorit.jpg" />
+                        <h3 name="carName">Chevrolet L030</h3>
+                        <h1 name="price">$99,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../img/auto9Kategorit.jpg" />
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="../img/auto9Kategorit.jpg" />
+                        <h3 name="carName">BMW X6 Series</h3>
+                        <h1 name="price">$129,90 per day</h1>
+                        <div class="car-booking">
+                            <a href="bookings.php">Book yours now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        </div>
+    </section>
+    <?php
+
+    include '../components/footer.php';
+
+    ?>
+</body>
+
+</html>
---
+++ views/ContactUs.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Contact Us</title>
+  <link rel="stylesheet" href="../css/ContactUsStyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <div class="container">
+      <div class="content">
+        <div class="left-side">
+          <div class="address details">
+            <i class="fas fa-map-marker-alt"></i>
+            <div class="topic">Address</div>
+            <div class="text-one">Prishtina, 10000</div>
+            <div class="text-two">Kosova 1923</div>
+          </div>
+          <div class="phone details">
+            <i class="fas fa-phone-alt"></i>
+            <div class="topic">Phone</div>
+            <div class="text-one">+383 49 222 333</div>
+            <div class="text-two">0800 20 300</div>
+          </div>
+          <div class="email details">
+            <i class="fas fa-envelope"></i>
+            <div class="topic">Email</div>
+            <div class="text-one">rentacar@gmail.com</div>
+            <div class="text-two">info.rentacar@gmail.com</div>
+          </div>
+        </div>
+        <div class="right-side">
+          <div class="topic-text">Send us a message</div>
+          <p></p>
+          <form action="#">
+            <div class="input-box">
+              <input id="name" type="text" placeholder="Enter your full name" required>
+            </div>
+            <div class="input-box">
+              <input id="emaili" type="text" placeholder="Enter your email" required>
+            </div>
+            <div class="input-box message-box">
+              <textarea id="textarea_message" placeholder="Your message for us..." required></textarea>
+            </div>
+            <div class="button">
+              <input type="button" value="Send Now" onclick="validationContactUs()">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+    <script src="../js/contact-us.js"></script>
+</body>
+
+</html>
